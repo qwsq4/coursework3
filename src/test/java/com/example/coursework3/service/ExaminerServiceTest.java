9950dec2e2fe2173dcc4024beb9e85db87d40be5
qwsq4 <qwsq1437@gmail.com>
2022-12-06 21:14:44 +0800

    1

--- src/test/java/com/example/coursework3/service/ExaminerServiceTest.java
+++ src/test/java/com/example/coursework3/service/ExaminerServiceTest.java
@@ -1,14 +1,17 @@
 package com.example.coursework3.service;
 
+import com.example.coursework3.exception.IllegalQuestionsAmount;
 import com.example.coursework3.model.Question;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,18 +26,19 @@ public class ExaminerServiceTest {
     @InjectMocks
     ExaminerServiceImpl examinerService;
 
-    @BeforeEach
-    void setup() {
-        List<Question> list = new ArrayList<>();
-        list.add(new Question("", ""));
+    @Test
+    void getQuestions() {
         Mockito.when(javaQuestionService.getCount()).thenReturn(1);
         Mockito.when(javaQuestionService.getRandomQuestion()).thenReturn(question);
+        List<Question> list = new ArrayList<>();
+        list.add(question);
+        Assertions.assertThat(examinerService.getQuestions(1)).isEqualTo(list);
     }
 
     @Test
-    void getQuestions() {
-        List<Question> list = new ArrayList<>();
-        list.add(question);
-        Assertions.assertThat(this.examinerService.getQuestions(1)).isEqualTo(list);
+    void throwException() {
+        Mockito.when(javaQuestionService.getCount()).thenReturn(0);
+        org.junit.jupiter.api.Assertions.assertThrows(IllegalQuestionsAmount.class,
+                () -> examinerService.getQuestions(1));
     }
 }
