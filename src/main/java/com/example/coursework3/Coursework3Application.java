dac823009445aef6d1f5314abb521c8a8791f419
qwsq4 <qwsq1437@gmail.com>
2022-12-06 17:44:18 +0800

    Initial commit

---
+++ src/main/java/com/example/coursework3/Coursework3Application.java
@@ -0,0 +1,13 @@
+package com.example.coursework3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Coursework3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Coursework3Application.class, args);
+    }
+
+}
